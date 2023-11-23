7934f2ea09204bca37a487a697ff4635e9d0d5e7
jasarck <jasirclt@gmail.com>
2023-11-23 01:11:34 +0000

    Intial commit

---
+++ src/test/java/com/demo/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
